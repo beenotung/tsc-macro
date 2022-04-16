5aa41cf24800a394825d780f8ccc3ccac1d1c940
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-04-16 23:38:03 +0800

    ignore examples folder from prettier

--- examples/src/reports.ts
+++ examples/src/reports.ts
@@ -40,56 +40,56 @@ export const reasons = [
   {
     Code: 'Nudity or pornography',
     Eng: 'Nudity or pornography',
-    Zh: '裸露或色情',
+    Zh: '裸露或色情'
   },
   {
     Code: 'Violence',
     Eng: 'Violence',
-    Zh: '暴力',
+    Zh: '暴力'
   },
   {
     Code: 'Harassment',
     Eng: 'Harassment',
-    Zh: '騷擾',
+    Zh: '騷擾'
   },
   {
     Code: 'Suicide or self-harm',
     Eng: 'Suicide or self-harm',
-    Zh: '自殺或自殘',
+    Zh: '自殺或自殘'
   },
   {
     Code: 'False news',
     Eng: 'False news',
-    Zh: '虛假新聞',
+    Zh: '虛假新聞'
   },
   {
     Code: 'Spam',
     Eng: 'Spam',
-    Zh: '垃圾訊息',
+    Zh: '垃圾訊息'
   },
   {
     Code: 'Unauthorized sales',
     Eng: 'Unauthorized sales',
-    Zh: '未經授權銷售',
+    Zh: '未經授權銷售'
   },
   {
     Code: 'Hate speech',
     Eng: 'Hate speech',
-    Zh: '仇恨言論',
+    Zh: '仇恨言論'
   },
   {
     Code: 'Terrorism',
     Eng: 'Terrorism',
-    Zh: '恐怖主義',
+    Zh: '恐怖主義'
   },
   {
     Code: 'Copyright infringement',
     Eng: 'Copyright infringement',
-    Zh: '侵犯版權',
+    Zh: '侵犯版權'
   },
   {
     Code: 'Other',
     Eng: 'Other (please specify)',
-    Zh: '其他 (請列明)',
+    Zh: '其他 (請列明)'
   },
 ]
